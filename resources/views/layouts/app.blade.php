ea98ebce78c07fc6faf0be48a07c076b3a6d7180
Lisa Guemmar <guemmarlisa@gmail.com>
2023-02-23 21:55:38 +0100

    Ajout du footer sur le site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,8 @@
         <main class="py-4">
             @yield('content')
         </main>
+        
+        @include('partials/footer')
     </div>
 </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="bg-light py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <p>© 2023 Othrillo. Tous droits réservés.</p>
+      </div>
+      <div class="col-md-6 d-flex justify-content-end">
+        <a class="mx-1" href="#">Mentions légales</a>
+        <a class="mx-1" href="#">Politique de confidentialité</a>
+      </div>
+    </div>
+  </div>
+</footer>
