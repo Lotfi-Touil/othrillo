ea9850e132bb755eed181585a05cd4cdf997ebe2
Lotfi-Touil <lotfi.touil7@gmail.com>
2023-02-23 20:34:37 +0100

    Ajout du header au site

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 
 @section('content')
@@ -5,16 +6,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    <h1>Bienvenue</h1>
+                </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <h4>Le site est en cours de dev :/</h4>
                 </div>
             </div>
         </div>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,27 @@
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">À propos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">S'inscrire</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Se connecter</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
