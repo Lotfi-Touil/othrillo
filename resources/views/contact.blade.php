a29a8e2caf98caea25beffe61633bad421e2b7e3
Lotfi-Touil <lotfi.touil7@gmail.com>
2023-02-27 01:14:21 +0100

    Création de la page contact

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="my-4">Contactez-nous</h1>
+
+        <form>
+            <div class="form-group">
+                <label for="name">Nom</label>
+                <input type="text" class="form-control" id="name" name="name" required value="{{ old('name') }}">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Adresse e-mail</label>
+                <input type="email" class="form-control" id="email" name="email" required value="{{ old('email') }}">
+            </div>
+
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" id="message" name="message" required>{{ old('message') }}</textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary mt-3">Envoyer</button>
+        </form>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', function () { return view('home'); });
+Route::get('/contact', function () { return view('contact'); });
