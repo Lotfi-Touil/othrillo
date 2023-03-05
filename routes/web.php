2657c8439afee728374a4682fbbdfc2a97bdaf2c
Lotfi-Touil <lotfi.touil7@gmail.com>
2023-03-05 15:53:25 +0100

    [CODE REVIEW]

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return view('home'); });
-Route::get('/contact', function () { return view('contact'); });
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/article', function () {
+    return view('article');
+});
