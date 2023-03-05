2cb51c05acb20b758729b45a5673f93c33d97844
Lotfi-Touil <lotfi.touil7@gmail.com>
2023-03-05 18:16:56 +0100

    Feature : permettre aux visiteurs de s'inscrire à une newsletter pour être tenus au courant de vos nouveaux articles

---
+++ resources/views/newsletter.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="my-4">Newsletter</h1>
+        {{-- développement de ce Issue : Feature : permettre aux visiteurs de s'inscrire à une newsletter pour être tenus au courant de vos nouveaux articles#17 --}}
+    </div>
+@endsection
