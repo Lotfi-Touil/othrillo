3d91999648ca69cfbe2ce65d309e9aca2e65be88
Lisa Guemmar <guemmarlisa@gmail.com>
2023-03-05 16:26:39 +0100

    Feature : Ajout d'une fonctionnalité de catégorisation pour organiser les articles sur le site

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -4,5 +4,7 @@
     <div class="container">
         <h1 class="my-4">Bienvenue dans la page article</h1>
         {{-- développement de ce Issue : Feature : Balises - ajouter des balises à vos articles pour faciliter la recherche et la découverte de contenu connexe#23 --}}
+
+        <!-- Feature : Ajout d'une fonctionnalité de catégorisation pour organiser les articles sur le site -->
     </div>
 @endsection
