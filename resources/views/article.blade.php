88e3fe409e3df7fadd05eeb9e21623f2454df46d
Lotfi-Touil <lotfi.touil7@gmail.com>
2023-03-05 16:42:16 +0100

    Bug: Certains articles ne s'affichent pas correctement sur mobile

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -5,6 +5,8 @@
         <h1 class="my-4">Bienvenue dans la page article</h1>
         {{-- développement de ce Issue : Feature : Balises - ajouter des balises à vos articles pour faciliter la recherche et la découverte de contenu connexe#23 --}}
 
-        <!-- Feature : Ajout d'une fonctionnalité de catégorisation pour organiser les articles sur le site -->
+        {{-- développement de ce Issue : Feature : Ajout d'une fonctionnalité de catégorisation pour organiser les articles sur le site --}}
+ 
+        {{-- développement de ce Issue : Bug: Certains articles ne s'affichent pas correctement sur mobile #29 --}}
     </div>
 @endsection
