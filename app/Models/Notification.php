9cf87211bc43245415867bed1f50046868422cd6
Lotfi-Touil <lotfi.touil7@gmail.com>
2023-03-05 18:03:57 +0100

    Feature : Notifications - Ajouter un système de notification pour informer les utilisateurs lorsqu'un nouvel article est publié ou lorsqu'un de leurs commentaires reçoit une réponse

---
+++ app/Models/Notification.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+    // développement de ce Issue : Feature : Notifications - Ajouter un système de notification pour informer les utilisateurs lorsqu'un nouvel article est publié ou lorsqu'un de leurs commentaires reçoit une réponse #43
+}
