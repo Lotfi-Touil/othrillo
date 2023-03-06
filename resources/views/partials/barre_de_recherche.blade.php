0735dec8be1faa8b370851616f38e164fe7cb287
Lisa Guemmar <guemmarlisa@gmail.com>
2023-03-06 15:57:08 +0100

    Feature : Barre de recherche améliorée : Ajouter une barre de recherche avancée pour permettre aux utilisateurs de trouver rapidement les articles qu'ils recherchent. #48

---
+++ resources/views/partials/barre_de_recherche.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="my-4">Bienvenue dans la page auteur</h1>
+        {{-- développement de ce Issue : Feature : Barre de recherche améliorée : Ajouter une barre de recherche avancée pour permettre aux utilisateurs de trouver rapidement les articles qu'ils recherchent. #48
+  --}}
+</div>
+@endsection
