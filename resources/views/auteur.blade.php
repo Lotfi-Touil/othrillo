dc3eb7591dd2b6228472aec794e3817c13ca23e4
Lisa Guemmar <guemmarlisa@gmail.com>
2023-03-06 15:47:48 +0100

    Feature : Page Auteur - Ajouter une page d'auteur qui présente l'auteur, ses publications et ses informations de contact.

---
+++ resources/views/auteur.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="my-4">Bienvenue dans la page article</h1>
+        {{-- développement de ce Issue : Feature : Balises - ajouter des balises à vos articles pour faciliter la recherche et la découverte de contenu connexe#23 --}}
+
+        {{-- développement de ce Issue : Feature : Ajout d'une fonctionnalité de catégorisation pour organiser les articles sur le site --}}
+ 
+        {{-- développement de ce Issue : Bug: Certains articles ne s'affichent pas correctement sur mobile #29 --}}
+
+        {{-- développement de ce Issue : Feature : Ajouter des boutons de partage sur les réseaux sociaux pour faciliter le partage de vos articles #16 --}}
+
+        {{-- développement de ce Issue : Feature : Permettre aux visiteurs de laisser des commentaires sur les articles #15 --}}
+    </div>
+@endsection
